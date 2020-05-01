7e56d756698f239ea7a6f7fab1949e4339344609
danny <m.valentin@students.ephec.be>
2020-05-01 02:43:56 +0200

    added helmet removed styles self

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(helmet());
 
 app.use(helmet.contentSecurityPolicy({
     directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'"]
+        defaultSrc: ["'self'"]
     }
 }));
 
