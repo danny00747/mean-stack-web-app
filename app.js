fbfc485151f0da506e053d2d9ea15f2a96577188
danny <morganz55057@gmail.com>
2020-07-06 04:53:47 +0200

    compiled es6 to es5 v2

--- app.js
+++ app.js
@@ -1,113 +1,110 @@
-"use strict";
-
-var _express = _interopRequireDefault(require("express"));
-
-var _bodyParser = _interopRequireDefault(require("body-parser"));
-
-var _cors = _interopRequireDefault(require("cors"));
-
-var _passport = _interopRequireDefault(require("passport"));
-
-var _passport2 = _interopRequireDefault(require("./dist/security/passport"));
-
-var _routes = _interopRequireDefault(require("./dist/routes"));
-
-var _path = _interopRequireDefault(require("path"));
-
-var _helmet = _interopRequireDefault(require("helmet"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import passport from 'passport'
+import authenticateUser from './server-side/security/passport';
+import routes from './server-side/routes'
 const addRequestId = require('express-request-id')();
-
-const {
-  success,
-  info,
-  error,
-  debug
-} = require('consola');
-
+import path from 'path';
+const {success, info, error, debug} = require('consola');
+import helmet from 'helmet';
 require('dotenv').config();
+require('./server-side/config/database');
 
-require('./dist/config/database'); // Initialize the app
+// Initialize the app
+const app = express();
 
+// Defining the Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(addRequestId);
 
-const app = (0, _express.default)(); // Defining the Middlewares
+// security headers
+app.use(helmet.hsts({
+    maxAge: 5184000,
+    includeSubDomains: true
+}));
 
-app.use((0, _cors.default)());
-app.use((0, _helmet.default)());
-app.use(addRequestId); // security headers 
+// security headers
+app.use(helmet.featurePolicy({
+    features: {
+        fullscreen: ["'self'"],
+        vibrate: ["'none'"],
+        payment: ["'none'"],
+        syncXhr: ["'none'"],
+        camera: ["'none'"],
+        microphone: ["'none'"],
+    }
+}));
 
-app.use(_helmet.default.hsts({
-  maxAge: 5184000,
-  includeSubDomains: true
-})); // security headers 
+// security headers
+app.use(helmet.permittedCrossDomainPolicies());
 
-app.use(_helmet.default.featurePolicy({
-  features: {
-    fullscreen: ["'self'"],
-    vibrate: ["'none'"],
-    payment: ["'none'"],
-    syncXhr: ["'none'"],
-    camera: ["'none'"],
-    microphone: ["'none'"]
-  }
-})); // security headers 
-
-app.use(_helmet.default.permittedCrossDomainPolicies()); // security headers 
-
-app.use(_helmet.default.referrerPolicy({
-  policy: 'strict-origin-when-cross-origin'
-})); // set the static folder
-
-if ((process && process.env && process.env.NODE_ENV || "production") === 'production') {
-  app.use(_express.default.static(_path.default.join(__dirname, 'server-side', 'public', 'dist')));
-} // BodyParser Midlleware
+// security headers
+app.use(helmet.referrerPolicy({policy: 'strict-origin-when-cross-origin'}));
 
 
-app.use(_bodyParser.default.json()); // Passport Middleware
+// set the static folder
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path
+        .join(__dirname, 'server-side', 'public', 'dist')));
+}
 
-app.use(_passport.default.initialize());
-app.use(_passport.default.session()); // bring the passport auth strategy
 
-(0, _passport2.default)(_passport.default);
+// BodyParser Midlleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// bring the passport auth strategy
+authenticateUser(passport);
+
 app.get('/', (req, res) => {
-  return res.sendFile(_path.default.join(__dirname + '/server-side', 'public', 'dist', 'index.html'));
-}); // Bring in the user routers
+    return res.sendFile(path
+        .join(__dirname + '/server-side', 'public', 'dist', 'index.html'));
+});
+
+// Bring in the user routers
 //const reviews = require('./server-side/routes/reviews');
 //const users = require('./server-side/routes/users');
 //const questions = require('./server-side/routes/questions');
+const logs = require('./server-side/routes/logs');
 
-const logs = require('./dist/routes/logs');
-
-app.use('/server/api/', _routes.default.usersRoutes);
-app.use('/server/api/', _routes.default.questionsRoutes);
-app.use('/server/api/', _routes.default.reviewsRoutes);
+app.use('/server/api/', routes.usersRoutes);
+app.use('/server/api/', routes.questionsRoutes);
+app.use('/server/api/', routes.reviewsRoutes);
 app.use('/server/api/', logs);
+
 app.use('/server/api/api-documentation', (req, res) => {
-  return res.sendFile(_path.default.join(__dirname + '/docs', 'index.html'));
+    return res.sendFile(path
+        .join(__dirname + '/docs', 'index.html'));
 });
+
 app.get('*', (req, res) => {
-  return res.sendFile(_path.default.join(__dirname + '/server-side', 'public', 'dist', 'index.html'));
+    return res.sendFile(path
+        .join(__dirname + '/server-side', 'public', 'dist', 'index.html'));
 });
-app.listen(process && process.env && process.env.PORT || "5000", () => {
-  info({
-    message: `Server started on port ${process && process.env && process.env.PORT || "5000"}`,
-    badge: true
-  });
-}); // catch 404 and forward to error handler
 
+app.listen(process.env.PORT, () => {
+    info({message: `Server started on port ${process.env["PORT"]}`, badge: true});
+});
+
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const error = new URIError("Page Not found !");
-  error.status = 404;
-  next(error.message);
-}); // Error Handler
-
-app.use((error, req, res) => {
-  //console.log(res.locals.error);
-  res.status(error.status || 500);
-  res.json({
-    message: error.message
-  });
+    const error = new Error("Page Not found !");
+    error.status = 404;
+    next(error.message);
 });
+
+// Error Handler
+app.use((error, req, res) => {
+    //console.log(res.locals.error);
+    res.status(error.status || 500);
+    res.json({
+        message: error.message
+    });
+});
+
 module.exports = app;
