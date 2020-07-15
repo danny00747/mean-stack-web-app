f9060a698abf7e113c03254251cac1d6e279fde9
danny <morganz55057@gmail.com>
2020-07-15 19:13:53 +0200

    added nodemailer

--- server-side/controllers/logs/get-logs.js
+++ server-side/controllers/logs/get-logs.js
@@ -1,5 +1,27 @@
+const sgMail = require('@sendgrid/mail');
 export default function makeGetLogsController({getAllLogService}) {
     return async (httpRequest) => {
+
+        sgMail.setApiKey(process.env["SENDGRID_API_KEY"]);
+        const msg = {
+            to: 'danbarca955@gmail.com',
+            from: 'shepalau007@gmail.com',
+            subject: 'Sending with Twilio SendGrid is Fun',
+            text: 'and easy to do anywhere, even with Node.js',
+            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        };
+
+        (async () => {
+            try {
+                await sgMail.send(msg);
+            } catch (error) {
+                console.error(error);
+
+                if (error.response) {
+                    console.error(error.response.body)
+                }
+            }
+        })();
 
         try {
 
