9e6b2a3588e6cc2b0641a2a0f1cd109661e7fa39
danny <morganz55057@gmail.com>
2020-08-07 05:22:32 +0200

    updated logs route

--- server-side/routes/logs.js
+++ server-side/routes/logs.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 import {logController} from '../controllers'
 import makeCallback from '../helpers/express-callback'
+import grantAccess from "../security/grandAccess";
 
-router.get("/users/metrics/max/:limit", makeCallback(logController.getLogs));
+router.get("/users/metrics/max/:limit",
+    passport.authenticate("jwt", {session: false}),
+    grantAccess('readAny', 'profile'),
+    makeCallback(logController.getLogs));
 
 export {router as logsRoutes};
